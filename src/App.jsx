141c7020d6432ca318e35834d1832a6b423eb462
enzoulloa <enzo_ulloa.i@gmail.com>
2023-02-02 03:44:41 -0300

    feat: :sparkles: add main and introduction section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from './layout/Header'
+import Main from './layout/Main'
 
 function App () {
   return (
     <>
       <Header />
+      <Main />
     </>
   )
 }
---
+++ src/layout/Main.jsx
@@ -0,0 +1,12 @@
+import Introduction from '../components/Introduction'
+
+const Main = () => {
+  return (
+    <main>
+      <Introduction />
+
+    </main>
+  )
+}
+
+export default Main
