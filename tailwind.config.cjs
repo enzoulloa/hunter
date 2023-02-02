2313aea3f3defed21e98465f79cc2b32f3d99c7c
enzoulloa <enzo_ulloa.i@gmail.com>
2023-02-02 03:30:36 -0300

    feat: :sparkles: add font-family and style configuration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,17 @@ module.exports = {
   content: ['./index.html',
     './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#021b29',
+        secondary: '#f2a70c',
+        primary_dark: '#01141f',
+        buttonText: '#333333'
+      },
+      fontFamily: {
+        Inter: ['Inter', 'sans serif']
+      }
+    }
   },
   plugins: []
 }
