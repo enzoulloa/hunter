bcee47a4d86821975e2ff01d7482ab9f5d4dec9a
enzoulloa <enzo_ulloa.i@gmail.com>
2023-02-02 20:00:58 -0300

    feat: :sparkles: add navMenu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,16 @@
+import { useState } from 'react'
+import BurgerIcon from './BurgerIcon'
+import NavMenu from './NavMenu'
 import logo from '../assets/icons/LogoIcon.svg'
 import text from '../assets/icons/HunterText.svg'
-import BurgerIcon from './BurgerIcon'
 
 const Navbar = () => {
+  const [isActive, setIsActive] = useState(false)
+
+  const handleClick = () => {
+    setIsActive(!isActive)
+  }
+
   return (
     <nav className=' w-full flex justify-between items-center '>
       <a className=' w-[130px] h-[60px] pl-0 flex overflow-hidden justify-start items-center rounded-full bg-secondary relative cursor-pointer' href=''>
@@ -10,13 +18,15 @@ const Navbar = () => {
         <img className=' w-[60px] left-[55px] max-w-full border-none absolute' src={text} alt='Hunter' />
       </a>
 
-      <button className=' w-[140px] h-[60px] rounded bg-secondary text-primary_dark text-sm font-extrabold tracking-widest cursor-pointer'>
+      <button onClick={handleClick} className={` w-[140px] h-[60px] bg-secondary text-primary_dark text-sm font-extrabold tracking-widest cursor-pointer ${isActive ? 'rounded-t' : ' rounded'}`}>
         <div className=' flex w-full h-full px-5 justify-between items-center'>
           <p className=' uppercase'>Menu</p>
-          <BurgerIcon />
+          <BurgerIcon toggle={isActive} />
         </div>
       </button>
-
+      {
+  isActive && <NavMenu />
+}
     </nav>
   )
 }
