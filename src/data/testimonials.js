58ecdd96f2cd90324ec5d353d0a43d95dcf95835
enzoulloa <enzo_ulloa.i@gmail.com>
2023-02-04 02:30:29 -0300

    feat: :sparkles: add testimonials data

---
+++ src/data/testimonials.js
@@ -0,0 +1,25 @@
+
+const person1 = {
+  name: 'Sophia Fraser',
+  paragraph: 'Hunter is a very professional and creative photographer. Hunter has plenty of ideas to make the pictures clear and interesting. We look forward to working with him again soon.',
+  company: 'R&T HOUSING',
+  image: 'https://assets.website-files.com/6005526df587c631c25dcda8/60097abca775f802178ba9e0_Testimonee%202.jpg'
+}
+const person2 = {
+  name: 'Alessandro Rutledge',
+  paragraph: 'We were looking for a photographer that could deliver corporate yet personal photos of our company and we’re happy to say that Hunter exceeded our expectations.',
+  company: 'CLEARSKY',
+  image: 'https://assets.website-files.com/6005526df587c631c25dcda8/60097abccf81a0dd19f32612_Testimonee%201.jpg'
+}
+const person3 = {
+  name: 'Max Duncan',
+  paragraph: 'I cant thank Hunter enough. As an interior designer it is a blast to work with Hunter. He masters capturing the "feeling" of a room and guides you through his process step by step.',
+  company: 'MD INTERIOR DESIGN',
+  image: 'https://assets.website-files.com/6005526df587c631c25dcda8/60097abb45504ce6c47b00ea_Testimonee%203.jpg'
+}
+const testimonials = [
+  person1,
+  person2,
+  person3
+]
+export { testimonials }
