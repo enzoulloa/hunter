f2dd5deef5c73ae0d99b7303a3eab60546b4b1b4
enzoulloa <enzo_ulloa.i@gmail.com>
2023-02-06 02:15:30 -0300

    feat: :sparkles: add logos and active transition

--- src/containers/Clients.jsx
+++ src/containers/Clients.jsx
@@ -15,40 +15,40 @@ const Clients = () => {
     <section ref={animateRef} id='clients' className=' bg-primary_dark py-20 px-5 w-full max-w-7xl mx-auto transition duration-700 ease-in opacity-0'>
       <div className=' grid gap-5 grid-cols-2 '>
         <div className=' h-[100px] w-full flex flex-col justify-center items-center rounded bg-primary'>
-          <img className=' w-[100px] opacity-50 max-w-full align-middle border-none' src='https://assets.website-files.com/6005526df587c631c25dcda8/60097c71ecc25a227696a572_Logo%20Ipsum.svg' alt='client-logo' />
+          <img className=' w-[100px] opacity-50 max-w-full align-middle border-none active:opacity-100' src='https://assets.website-files.com/6005526df587c631c25dcda8/60097c71ecc25a227696a572_Logo%20Ipsum.svg' alt='client-logo' />
         </div>
         <div className=' h-[100px] w-full flex flex-col justify-center items-center rounded bg-primary'>
-          <img className=' w-[100px] opacity-50 max-w-full align-middle border-none' src='https://assets.website-files.com/6005526df587c631c25dcda8/60097c71ecc25a227696a572_Logo%20Ipsum.svg' alt='client-logo' />
+          <img className=' w-[100px] opacity-50 max-w-full align-middle border-none active:opacity-100' src='https://assets.website-files.com/6005526df587c631c25dcda8/60097c84e2459050b1cac87b_Logo%20Ipsum%203.svg' alt='client-logo' />
         </div>
         <div className=' h-[100px] w-full flex flex-col justify-center items-center rounded bg-primary'>
-          <img className=' w-[100px] opacity-50 max-w-full align-middle border-none' src='https://assets.website-files.com/6005526df587c631c25dcda8/60097c71ecc25a227696a572_Logo%20Ipsum.svg' alt='client-logo' />
+          <img className=' w-[100px] opacity-50 max-w-full align-middle border-none active:opacity-100' src='https://assets.website-files.com/6005526df587c631c25dcda8/60097c84b0ac197fa5cb6c59_Logo%20Ipsum%204.svg' alt='client-logo' />
         </div>
         <div className=' h-[100px] w-full flex flex-col justify-center items-center rounded bg-primary'>
-          <img className=' w-[100px] opacity-50 max-w-full align-middle border-none' src='https://assets.website-files.com/6005526df587c631c25dcda8/60097c71ecc25a227696a572_Logo%20Ipsum.svg' alt='client-logo' />
+          <img className=' w-[100px] opacity-50 max-w-full align-middle border-none active:opacity-100' src='https://assets.website-files.com/6005526df587c631c25dcda8/60097c84a0951e8ef85b92a5_Logo%20Ipsum%202.svg' alt='client-logo' />
         </div>
         <div className=' h-[100px] w-full flex flex-col justify-center items-center rounded bg-primary'>
-          <img className=' w-[100px] opacity-50 max-w-full align-middle border-none' src='https://assets.website-files.com/6005526df587c631c25dcda8/60097c71ecc25a227696a572_Logo%20Ipsum.svg' alt='client-logo' />
+          <img className=' w-[100px] opacity-50 max-w-full align-middle border-none active:opacity-100' src='https://assets.website-files.com/6005526df587c631c25dcda8/60097c84e2459050b1cac87b_Logo%20Ipsum%203.svg' alt='client-logo' />
         </div>
         <div className=' h-[100px] w-full flex flex-col justify-center items-center rounded bg-primary'>
-          <img className=' w-[100px] opacity-50 max-w-full align-middle border-none' src='https://assets.website-files.com/6005526df587c631c25dcda8/60097c71ecc25a227696a572_Logo%20Ipsum.svg' alt='client-logo' />
+          <img className=' w-[100px] opacity-50 max-w-full align-middle border-none active:opacity-100' src='https://assets.website-files.com/6005526df587c631c25dcda8/60097c84b0ac197fa5cb6c59_Logo%20Ipsum%204.svg' alt='client-logo' />
         </div>
         <div className=' h-[100px] w-full flex flex-col justify-center items-center rounded bg-primary'>
-          <img className=' w-[100px] opacity-50 max-w-full align-middle border-none' src='https://assets.website-files.com/6005526df587c631c25dcda8/60097c71ecc25a227696a572_Logo%20Ipsum.svg' alt='client-logo' />
+          <img className=' w-[100px] opacity-50 max-w-full align-middle border-none active:opacity-100' src='https://assets.website-files.com/6005526df587c631c25dcda8/60097c84a0951e8ef85b92a5_Logo%20Ipsum%202.svg' alt='client-logo' />
         </div>
         <div className=' h-[100px] w-full flex flex-col justify-center items-center rounded bg-primary'>
-          <img className=' w-[100px] opacity-50 max-w-full align-middle border-none' src='https://assets.website-files.com/6005526df587c631c25dcda8/60097c71ecc25a227696a572_Logo%20Ipsum.svg' alt='client-logo' />
+          <img className=' w-[100px] opacity-50 max-w-full align-middle border-none active:opacity-100' src='https://assets.website-files.com/6005526df587c631c25dcda8/60097c71ecc25a227696a572_Logo%20Ipsum.svg' alt='client-logo' />
         </div>
         <div className=' h-[100px] w-full flex flex-col justify-center items-center rounded bg-primary'>
-          <img className=' w-[100px] opacity-50 max-w-full align-middle border-none' src='https://assets.website-files.com/6005526df587c631c25dcda8/60097c71ecc25a227696a572_Logo%20Ipsum.svg' alt='client-logo' />
+          <img className=' w-[100px] opacity-50 max-w-full align-middle border-none active:opacity-100' src='https://assets.website-files.com/6005526df587c631c25dcda8/60097c71ecc25a227696a572_Logo%20Ipsum.svg' alt='client-logo' />
         </div>
         <div className=' h-[100px] w-full flex flex-col justify-center items-center rounded bg-primary'>
-          <img className=' w-[100px] opacity-50 max-w-full align-middle border-none' src='https://assets.website-files.com/6005526df587c631c25dcda8/60097c71ecc25a227696a572_Logo%20Ipsum.svg' alt='client-logo' />
+          <img className=' w-[100px] opacity-50 max-w-full align-middle border-none active:opacity-100' src='https://assets.website-files.com/6005526df587c631c25dcda8/60097c84a0951e8ef85b92a5_Logo%20Ipsum%202.svg' alt='client-logo' />
         </div>
         <div className=' h-[100px] w-full flex flex-col justify-center items-center rounded bg-primary'>
-          <img className=' w-[100px] opacity-50 max-w-full align-middle border-none' src='https://assets.website-files.com/6005526df587c631c25dcda8/60097c71ecc25a227696a572_Logo%20Ipsum.svg' alt='client-logo' />
+          <img className=' w-[100px] opacity-50 max-w-full align-middle border-none active:opacity-100' src='https://assets.website-files.com/6005526df587c631c25dcda8/60097c84e2459050b1cac87b_Logo%20Ipsum%203.svg' alt='client-logo' />
         </div>
         <div className=' h-[100px] w-full flex flex-col justify-center items-center rounded bg-primary'>
-          <img className=' w-[100px] opacity-50 max-w-full align-middle border-none' src='https://assets.website-files.com/6005526df587c631c25dcda8/60097c71ecc25a227696a572_Logo%20Ipsum.svg' alt='client-logo' />
+          <img className=' w-[100px] opacity-50 max-w-full align-middle border-none active:opacity-100' src='https://assets.website-files.com/6005526df587c631c25dcda8/60097c84b0ac197fa5cb6c59_Logo%20Ipsum%204.svg' alt='client-logo' />
         </div>
       </div>
       <Testimonials />
