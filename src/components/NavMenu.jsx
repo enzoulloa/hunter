e58d91f072e6a49c8193166a72c1770436941589
enzoulloa <enzo_ulloa.i@gmail.com>
2023-02-06 04:22:53 -0300

    feat: :iphone: add responsive style

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,7 +1,7 @@
 const NavMenu = ({ active }) => {
   return (
-    <div className={`  absolute top-full left-0 right-0 w-full  overflow-hidden transition ${active ? 'animate-heightAnimation' : 'animate-heightAnimationEnd'} `}>
-      <div className=' top-0 right-5 min-w-[140px] left-auto bottom-auto py-[10px] rounded-bl rounded-br-[20px] bg-secondary text-primary_dark font-semibold absolute text-left h-auto flex flex-col items-start justify-start'>
+    <div className={`  absolute top-full left-0 right-0 w-full overflow-hidden transition ${active ? 'animate-heightAnimation' : 'animate-heightAnimationEnd'} `}>
+      <div className=' top-0 right-5 min-w-[140px] left-auto bottom-auto py-[10px] rounded-bl rounded-br-[20px] bg-secondary text-primary_dark font-semibold absolute text-left h-auto flex flex-col items-start justify-start md:right-10 md:min-w-[160px]'>
         <a className=' py-[10px] relative no-underline align-top px-5 text-left block focus:underline' href='#works'>Portfolio</a>
         <a className=' py-[10px] relative no-underline align-top  px-5 text-left block focus:underline' href='#about'>About me</a>
         <a className=' py-[10px] relative no-underline align-top px-5 text-left block focus:underline' href='#clients'>Clients</a>
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,7 +2,7 @@ import Navbar from '../components/Navbar'
 
 const Header = () => {
   return (
-    <header className=' fixed left-0 top-5 right-0 bottom-auto w-full px-5 z-30'>
+    <header className=' fixed left-0 top-5 right-0 bottom-auto w-full px-5 z-30 md:top-10 md:px-10'>
       <Navbar />
     </header>
   )
