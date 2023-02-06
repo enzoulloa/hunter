0e61a9e6848b89707eee3d13e17c6cf11911bfe0
enzoulloa <enzo_ulloa.i@gmail.com>
2023-02-06 04:23:33 -0300

    feat: :lipstick: add tablet responsive style

--- src/containers/Introduction.jsx
+++ src/containers/Introduction.jsx
@@ -12,16 +12,16 @@ const Introduction = () => {
   }
 
   return (
-    <section ref={animateRef} id='introduction' className=' pt-40 px-5 flex w-full mx-auto flex-col justify-center items-start max-w-7xl transition duration-700 ease-in opacity-0 '>
-      <div className=' flex w-full max-w-2xl flex-col items-center'>
-        <h1 className='text-[40px] leading-[48px] mt-0 mb-5 font-bold'>Hi! I'm Hunter, <br />Freelance photograper </h1>
-        <p className=' mt-3 text-base font-medium leading-7 text-white opacity-50'>Based in Amsterdam, I specialize in conceptual photography and love to take photos with creativity and passion. </p>
+    <section ref={animateRef} id='introduction' className=' pt-40 px-5 md:pt-[180px] md:px-10 flex w-full mx-auto flex-col justify-center items-start max-w-7xl transition duration-700 ease-in opacity-0 '>
+      <div className=' flex w-full flex-col items-start max-w-[600px]'>
+        <h1 className='text-[40px] leading-[48px] mt-0 mb-5 font-bold md:text-[64px] md:leading-[76px] '>Hi! I'm Hunter, <br />Freelance photograper </h1>
+        <p className=' mt-3 text-base font-medium leading-7 text-white opacity-50 md:text-[22px] md:leading-[1.8]'>Based in Amsterdam, I specialize in conceptual photography and love to take photos with creativity and passion. </p>
+        <a className=' flex items-center mt-10 px-10 py-4 text-center align-middle rounded bg-secondary text-sm text-primary_dark tracking-widest uppercase font-extrabold' href='#works'>Explore my work
+          <span className='material-symbols-outlined '>
+            keyboard_arrow_right
+          </span>
+        </a>
       </div>
-      <a className=' flex items-center mt-10 px-10 py-4 text-center align-middle rounded bg-secondary text-sm text-primary_dark tracking-widest uppercase font-extrabold' href='#works'>Explore my work
-        <span className='material-symbols-outlined '>
-          keyboard_arrow_right
-        </span>
-      </a>
       <img className=' mt-10 opacity-80 ' src={img} alt='' />
     </section>
   )
