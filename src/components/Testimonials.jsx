dd360f6c8de8da46a9e1d311ba5ddc577de3a1d0
enzoulloa <enzo_ulloa.i@gmail.com>
2023-02-06 17:23:05 -0300

    feat: :lipstick: add responsive tablet view on clients section

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -23,34 +23,34 @@ const Testimonials = () => {
   }
 
   return (
-    <section className=' mt-20 max-w-3xl mx-auto text-center relative '>
-      <div className='z-10 left-0 right-0 block h-full whitespace-nowrap relative '>
-        <div className=' align-top w-full h-full whitespace-normal text-left flex gap-12 overflow-hidden p-4'>
+    <section className=' mt-20 max-w-[800px] mx-auto text-center relative md:mt-[100px]'>
+      <div className=' h-full relative w-full pb-[80px] text-center '>
+        <div className=' w-full h-full whitespace-nowrap  gap-12 overflow-hidden p-4 relative left-0 right-0 z-10'>
 
-          <div className='flex flex-col justify-start items-center w-full shrink-0 '>
+          <div className=' relative align-top whitespace-normal text-left flex flex-col justify-start items-center w-full shrink-0 h-[235px]'>
             <p className=' mb-5 text-2xl italic text-center font-normal leading-[1.788]'>{selectedTest.paragraph}</p>
             <div className=' flex items-center'>
-              <img className=' overflow-hidden w-10 h-10 mr-2 rounded-full object-cover' src={selectedTest.image} alt='' />
+              <img className=' overflow-hidden w-10 h-10 mr-[10px] rounded-full object-cover' src={selectedTest.image} alt='' />
               <div>
-                <p className=' mb-1 font-medium'>{selectedTest.name}</p>
-                <p className=' text-white text-[10px] text-opacity-50 font-bold tracking-widest uppercase'>{selectedTest.comp}</p>
+                <p className=' mb-[2px] font-medium'>{selectedTest.name}</p>
+                <p className=' text-white text-[10px] text-opacity-50 font-bold tracking-widest uppercase'>{selectedTest.company}</p>
               </div>
             </div>
           </div>
 
         </div>
-        <div className=' mx-auto w-4/5 flex justify-evenly items-center mt-10'>
-          <button onClick={previous} className=' w-20 h-8 rounded-full bg-secondary text-primary_dark m-auto cursor-pointer text-4xl flex justify-center items-center'>
-            <span className='material-symbols-outlined'>
-              arrow_back_ios
-            </span>
-          </button>
-          <button onClick={next} className=' w-20 h-8 rounded-full bg-secondary text-primary_dark m-auto cursor-pointer text-4xl flex justify-center items-center'>
-            <span className='material-symbols-outlined'>
-              navigate_next
-            </span>
-          </button>
-        </div>
+
+        <button onClick={previous} className=' absolute right-[53%] top-auto bottom-0 left-auto w-20 h-8 rounded-full bg-secondary text-primary_dark m-auto cursor-pointer'>
+          <span class='material-symbols-outlined'>
+            navigate_before
+          </span>
+        </button>
+        <button onClick={next} className=' absolute left-[53%] top-auto bottom-0 right-auto w-20 h-8 rounded-full bg-secondary text-primary_dark m-auto cursor-pointer  '>
+          <span className='material-symbols-outlined'>
+            navigate_next
+          </span>
+        </button>
+
       </div>
 
       <img className='absolute left-auto top-0 right-0 bottom-auto w-[60px] opacity-20' src={coma} alt='coma' />
