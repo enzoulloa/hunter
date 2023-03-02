17f3e200fef401006fa8e078668cfd535dcd7758
enzoulloa <enzo_ulloa.i@gmail.com>
2023-03-01 23:38:36 -0300

    fix: :bug: fix h2

--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -17,7 +17,7 @@ const About = () => {
           <img className=' w-full max-w-md object-cover align-middle -rotate-12' src='https://assets.website-files.com/6005526df587c631c25dcda8/6009822a840944eb78c55198_About%20Image%202.jpg' alt='' />
         </div>
         <div className=' -order-12 flex flex-col items-start md:order-1'>
-          <h2 className=' text-4xl mb-5 font-bold text-[56px] leading-[60px]'>Why I fell in love with photography</h2>
+          <h2 className=' text-4xl mb-5 font-bold text-[36px] leading-[42px] md:text-[56px] md:leading-[60px]'>Why I fell in love with photography</h2>
           <p className=' mt-5 text-opacity-50 text-white leading-7'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, <span className=' text-secondary'>eros dolor interdum nulla</span>, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.
             <br />
             <br />
