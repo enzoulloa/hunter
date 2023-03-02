b7ba46e250737d1b2ba12630a86b6f8267cb5f0c
enzoulloa <enzo_ulloa.i@gmail.com>
2023-03-02 17:23:18 -0300

    feat: :iphone: add tablet and laptop responsive styles

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -23,18 +23,21 @@ const Slider = ({ image, modalState, setModalState, images }) => {
     <>
       {modalState &&
         <div className=' w-screen h-screen fixed top-0 left-0 bg-black bg-opacity-90 flex items-center justify-center z-50'>
-          <div className=' w-[86vh] shadow-xl'>
-            <button onClick={onClick} className=' absolute top-3 right-1 w-7 h-7 cursor-pointer rounded text-white '>
-              <svg stroke='currentColor' fill='currentColor' stroke-width='0' viewBox='0 0 24 24' height='1em' width='1em' xmlns='http://www.w3.org/2000/svg'>
+          <div className=' w-[86vh] shadow-xl mx-auto flex items-center justify-center md:w-[82.3vw] md:h-[84vh]'>
+            <button onClick={onClick} className=' absolute top-3 right-1 lg:right-10 w-7 h-7 cursor-pointer rounded text-white '>
+              <svg className='lg:hidden' stroke='currentColor' fill='currentColor' stroke-width='0' viewBox='0 0 24 24' height='32px' width='32px' xmlns='http://www.w3.org/2000/svg'>
+                <path fill='none' stroke='#fff' stroke-width='3' d='M3,3 L21,21 M3,21 L21,3' />
+              </svg>
+              <svg className='hidden lg:block' stroke='currentColor' fill='currentColor' stroke-width='0' viewBox='0 0 24 24' height='46px' width='46px' xmlns='http://www.w3.org/2000/svg'>
                 <path fill='none' stroke='#fff' stroke-width='3' d='M3,3 L21,21 M3,21 L21,3' />
               </svg>
             </button>
-            <img src={selectedImage} alt='image' loading='lazy' />
+            <img className='w-full h-auto' src={selectedImage} alt='image' loading='lazy' />
             <div ref={carrusel} className=' flex gap-4 p-4 w-full snap-mandatory overflow-x-auto scroll-p-3 absolute bottom-0 scroll-smooth'>
               {
                 images.map((image, index) => {
                   return (
-                    <figure onClick={() => handleClick(index)} className=' w-28 h-24 rounded overflow-hidden shadow-lg shrink-0 snap-center cursor-pointer ' key={index}>
+                    <figure onClick={() => handleClick(index)} className=' w-28 h-24 rounded overflow-hidden shadow-lg shrink-0 snap-center cursor-pointer md:w-44 md:h-32 lg:w-96 lg:h-80' key={index}>
                       <img className=' object-cover w-full h-full' src={image} alt={modalState + ' image'} />
                     </figure>
                   )
